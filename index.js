92dc56120fc9140dcf56308c802bed1bd1de8fa4
V4Szz <lfbaldisseracps@gmail.com>
2024-09-09 19:15:48 -0300

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Playing VALORANT"];
+const statusMessages = ["Listening to Cassino","Playing By V4"];
 
 
 let currentIndex = 0;
